0039b23b11e9d036f09aa08c90b606235e421bdd
unknown <leonardohltp@gmail.com>
2023-03-06 19:47:48 -0300

    operador 06-03

---
+++ operador.py
@@ -0,0 +1,15 @@
+adicao = 2 + 2 
+subtracao = 2-2
+multiplicacao = 2*2
+divisao = 10/3
+divisao_inteiro = 10/3
+modulo = 10 % 4
+potencia = 2**3
+
+print(adicao)
+print(subtracao)
+print('A multiplicação é ', multiplicacao)
+print('A divisão é', divisao)
+print('A divisão é', divisao_inteiro)
+print('O modulo é', modulo)
+print('A potência é', potencia)
